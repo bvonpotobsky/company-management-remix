5a1777d969bc10e6780fc5dfe1d064b19f09beb1
Benjamin von Potobsky <bvonpotobsky@gmail.com>
2023-08-17 11:30:49 +1000

    refactor login-join pages

--- app/routes/join.tsx
+++ app/routes/join.tsx
@@ -1,17 +1,23 @@
 import type {ActionArgs, LoaderArgs, V2_MetaFunction} from "@remix-run/node";
 import {json, redirect} from "@remix-run/node";
 import {Form, Link} from "@remix-run/react";
-import {zodResolver} from "@hookform/resolvers/zod";
-import {getValidatedFormData, useRemixForm} from "remix-hook-form";
 
-// import {type NewUser, NewUserSchema} from "~/models/user.server";
+import {RemixFormProvider, getValidatedFormData, useRemixForm} from "remix-hook-form";
+import {zodResolver} from "@hookform/resolvers/zod";
+import {z} from "zod";
+
+import {cn} from "~/utils";
+import {format} from "date-fns";
+import {CalendarIcon} from "lucide-react";
 
 import {createUser, getUserByEmail} from "~/models/user.server";
 import {createUserSession, getUserId} from "~/session.server";
 
+import {Button} from "~/components/ui/button";
+import {Calendar} from "~/components/ui/calendar";
 import {Input} from "~/components/ui/input";
-import {Label} from "~/components/ui/label";
-import {z} from "zod";
+import {FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage} from "~/components/ui/form";
+import {Popover, PopoverContent, PopoverTrigger} from "~/components/ui/popover";
 
 export const NewUserSchema = z
   .object({
@@ -85,54 +91,126 @@ export default function Join() {
     resolver,
   });
 
-  const errors = form.formState.errors;
-
   return (
     <div className="flex min-h-full flex-col justify-center">
       <div className="mx-auto w-full max-w-md px-8">
-        <Form onSubmit={form.handleSubmit} className="last:mb-0 [&>*]:mb-3">
-          <div className="flex flex-col items-stretch space-y-2">
-            <Label>Full Name:</Label>
-            <Input type="text" {...form.register("name")} />
-            {errors.name && <p className="text-sm text-red-500">{errors.name.message}</p>}
-          </div>
-          <div className="flex flex-col items-stretch space-y-2">
-            <Label>Date of Birth:</Label>
-            <Input type="datetime-local" {...form.register("dob", {valueAsDate: true})} />
-            {errors.dob && <p className="text-sm text-red-500">{errors.dob.message}</p>}
-          </div>
-          <div className="flex flex-col items-stretch space-y-2">
-            <Label>Phone:</Label>
-            <Input type="tel" {...form.register("phone")} />
-            {errors.phone && <p className="text-sm text-red-500">{errors.phone.message}</p>}
-          </div>
-          <div className="flex flex-col items-stretch space-y-2">
-            <Label>Email:</Label>
-          </div>
-          <div className="flex flex-col items-stretch space-y-2">
-            <Input type="email" {...form.register("email")} />
-            {errors.email && <p className="text-sm text-red-500">{errors.email.message}</p>}
-            <Label>Password:</Label>
-          </div>
-          <div className="flex flex-col items-stretch space-y-2">
-            <Input type="password" {...form.register("password")} />
-            {errors.password && <p className="text-sm text-red-500">{errors.password.message}</p>}
-          </div>
-          <button
-            type="submit"
-            className="w-full rounded bg-blue-500 px-4 py-2 text-white hover:bg-blue-600 focus:bg-blue-400"
-          >
-            Create Account
-          </button>
-          <div className="flex items-center justify-center">
-            <div className="text-center text-sm text-gray-500">
-              Already have an account?{" "}
-              <Link className="text-blue-500 underline" to={{pathname: "/login"}}>
-                Log in
-              </Link>
+        <RemixFormProvider {...form}>
+          <Form onSubmit={form.handleSubmit} className="last:mb-0 [&>*]:mb-3">
+            <div className="flex flex-col items-stretch space-y-2">
+              <FormField
+                control={form.control}
+                name="name"
+                render={({field}) => (
+                  <FormItem className="flex flex-col">
+                    <FormLabel className="text-white">Full Name</FormLabel>
+                    <Input type="text" {...field} />
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
             </div>
-          </div>
-        </Form>
+            <div className="flex flex-col items-stretch space-y-2">
+              <FormField
+                control={form.control}
+                name="dob"
+                render={({field}) => (
+                  <FormItem className="flex flex-col">
+                    <FormLabel className="text-white">Date of birth</FormLabel>
+                    <Popover>
+                      <PopoverTrigger asChild>
+                        <FormControl>
+                          <Button
+                            variant={"outline"}
+                            className={cn(
+                              "w-[240px] pl-3 text-left font-normal",
+                              !field.value && "text-muted-foreground"
+                            )}
+                          >
+                            {field.value ? format(field.value, "PPP") : <span>Pick a date</span>}
+                            <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                          </Button>
+                        </FormControl>
+                      </PopoverTrigger>
+                      <PopoverContent className="w-auto p-0" align="start">
+                        <Calendar
+                          mode="single"
+                          selected={field.value}
+                          onSelect={field.onChange}
+                          disabled={(date) => date > new Date() || date < new Date("1900-01-01")}
+                          initialFocus
+                          captionLayout="dropdown"
+                          fromYear={1920}
+                          toYear={new Date().getFullYear()}
+                          classNames={{
+                            caption_label: "hidden",
+                            dropdown: "flex items-center space-x-2 bg-background",
+                            vhidden: "hidden",
+                            caption_dropdowns: "flex items-center space-x-2 bg-background text-white",
+                          }}
+                        />
+                      </PopoverContent>
+                    </Popover>
+                    <FormDescription>Your date of birth is used to calculate your age.</FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+            <div className="flex flex-col items-stretch space-y-2">
+              <FormField
+                control={form.control}
+                name="phone"
+                render={({field}) => (
+                  <FormItem className="flex flex-col">
+                    <FormLabel className="text-white">Phone</FormLabel>
+                    <Input type="tel" {...field} />
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+            <div className="flex flex-col items-stretch space-y-2">
+              <FormField
+                control={form.control}
+                name="email"
+                render={({field}) => (
+                  <FormItem className="flex flex-col">
+                    <FormLabel className="text-white">Email</FormLabel>
+                    <Input type="email" {...field} />
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+            <div className="flex flex-col items-stretch space-y-2">
+              <FormField
+                control={form.control}
+                name="password"
+                render={({field}) => (
+                  <FormItem className="flex flex-col">
+                    <FormLabel className="text-white">Password</FormLabel>
+                    <Input type="password" {...field} />
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+            <button
+              type="submit"
+              className="w-full rounded bg-blue-500 px-4 py-2 text-white hover:bg-blue-600 focus:bg-blue-400"
+            >
+              Create Account
+            </button>
+            <div className="flex items-center justify-center">
+              <div className="text-center text-sm text-gray-500">
+                Already have an account?{" "}
+                <Link className="text-blue-500 underline" to={{pathname: "/login"}}>
+                  Log in
+                </Link>
+              </div>
+            </div>
+          </Form>
+        </RemixFormProvider>
       </div>
     </div>
   );
--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -1,24 +1,23 @@
 import type {ActionArgs, LoaderArgs, V2_MetaFunction} from "@remix-run/node";
 import {json, redirect} from "@remix-run/node";
 import {Form, Link} from "@remix-run/react";
+
+import {z} from "zod";
+import {RemixFormProvider, getValidatedFormData, useRemixForm} from "remix-hook-form";
 import {zodResolver} from "@hookform/resolvers/zod";
 
 import {verifyLogin} from "~/models/user.server";
 import {createUserSession, getUserId} from "~/session.server";
-import {getValidatedFormData, useRemixForm} from "remix-hook-form";
 
-// import {type Login, LoginSchema} from "~/models/user.server";
-
-import {Input} from "~/components/ui/input";
-import {Label} from "@radix-ui/react-dropdown-menu";
 import {Button} from "~/components/ui/button";
 import {Checkbox} from "~/components/ui/checkbox";
-import {z} from "zod";
+import {Input} from "~/components/ui/input";
+import {FormField, FormItem, FormLabel, FormMessage} from "~/components/ui/form";
 
 export const LoginSchema = z
   .object({
-    email: z.string().email(),
-    password: z.string().min(8),
+    email: z.string().email({message: "Insert a valid email address"}),
+    password: z.string().min(8, {message: "Password must be at least 8 characters long"}),
     remember: z.boolean(),
   })
   .required();
@@ -34,9 +33,7 @@ export const loader = async ({request}: LoaderArgs) => {
 };
 
 export const action = async ({request}: ActionArgs) => {
-  const {data, errors, receivedValues} = await getValidatedFormData<Login>(request, resolver);
-
-  console.log({data, errors, receivedValues});
+  const {data, errors} = await getValidatedFormData<Login>(request, resolver);
 
   if (errors) {
     return json({errors});
@@ -52,8 +49,8 @@ export const action = async ({request}: ActionArgs) => {
   const isAdmin = user?.roles.find((role) => role.name === "admin");
 
   return createUserSession({
-    redirectTo: isAdmin ? "/admin" : "/",
-    remember: true,
+    redirectTo: isAdmin ? "/admin" : "/employee",
+    remember: data.remember,
     request,
     userId: user.id,
   });
@@ -71,41 +68,63 @@ export default function LoginPage() {
     },
   });
 
-  const errors = form.formState.errors;
-
-  console.log({errors});
-
   return (
     <div className="flex min-h-full flex-col justify-center">
       <div className="mx-auto w-full max-w-md px-8">
-        <Form onSubmit={form.handleSubmit} className="last:mb-0 [&>*]:mb-2">
-          <div className="flex flex-col items-stretch space-y-2">
-            <Label>Email</Label>
-            <Input type="email" {...form.register("email")} />
-            {errors.email && <p>{errors.email.message}</p>}
-          </div>
-          <div className="flex flex-col space-y-2">
-            <Label>Password:</Label>
-            <Input type="password" {...form.register("password")} />
-            {errors.email && <p>{errors.email.message}</p>}
-          </div>
+        <RemixFormProvider {...form}>
+          <Form onSubmit={form.handleSubmit} className="last:mb-0 [&>*]:mb-2">
+            <div className="flex flex-col items-stretch space-y-2">
+              <FormField
+                control={form.control}
+                name="email"
+                render={({field}) => (
+                  <FormItem>
+                    <FormLabel className="text-white">Email</FormLabel>
+                    <Input type="email" {...field} />
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+            <div className="flex flex-col space-y-2">
+              <FormField
+                control={form.control}
+                name="password"
+                render={({field}) => (
+                  <FormItem>
+                    <FormLabel className="text-white">Password</FormLabel>
+                    <Input type="password" {...field} />
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
 
-          <div className="flex items-center justify-start space-x-2">
-            <Checkbox {...form.register("remember")} />
-            <Label className="w-full">Remember me</Label>
-          </div>
+            <FormField
+              control={form.control}
+              name="remember"
+              render={() => (
+                <FormItem>
+                  <div className="flex items-center justify-start space-x-2">
+                    <Checkbox {...form.register("remember")} />
+                    <FormLabel className="text-white">Remember me</FormLabel>
+                  </div>
+                </FormItem>
+              )}
+            />
 
-          <Button variant="secondary" className="w-full" type="submit">
-            Submit
-          </Button>
+            <Button variant="secondary" className="w-full" type="submit">
+              Submit
+            </Button>
 
-          <div className="text-end text-sm">
-            Don't have an account?{" "}
-            <Link className="text-blue-500 underline" to={{pathname: "/join"}}>
-              Sign up
-            </Link>
-          </div>
-        </Form>
+            <div className="text-end text-sm">
+              Don't have an account?{" "}
+              <Link className="text-blue-500 underline" to={{pathname: "/join"}}>
+                Sign up
+              </Link>
+            </div>
+          </Form>
+        </RemixFormProvider>
       </div>
     </div>
   );
